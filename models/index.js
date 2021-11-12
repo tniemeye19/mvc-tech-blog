f5dcc3291a0889bedab1f6d99791954f6308f698
tniemeye19 <timothy.niemeyer19@gmail.com>
2021-11-12 09:02:55 -0500

    created associations between models

--- models/index.js
+++ models/index.js
@@ -1,5 +1,31 @@
+// Import the models used for associations
 const Comment = require('./Comment');
 const Post = require('./Post');
 const User = require('./User');
 
-// Need to Create associations for the models
+// Create associations between the models
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Comment }
